23d17b44fd82239eb4990f6a3b55f916b3dda4a8
Stilian Iliev <74832022+stilian-iliev@users.noreply.github.com>
2022-06-27 16:03:31 +0300

    The big bang

---
+++ src/main/java/com/softcloud/SoftcloudApplication.java
@@ -0,0 +1,13 @@
+package com.softcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftcloudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/softcloud/models/User.java
@@ -0,0 +1,40 @@
+package com.softcloud.models;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Type(type = "uuid-char")
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false)
+    private String password;
+
+    //list of liked songs
+    //list of following creators
+    //type band creator enjoyer
+    //list of uploaded songs
+
+    private String imageUrl;
+}
