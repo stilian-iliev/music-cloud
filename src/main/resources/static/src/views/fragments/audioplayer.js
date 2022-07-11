76754db00cedde051ecd56ee84c8d50a4a14d32d
Stilian Iliev <74832022+stilian-iliev@users.noreply.github.com>
2022-07-11 03:17:23 +0300

    audio player js

---
+++ src/main/resources/static/src/views/fragments/audioplayer.js
@@ -0,0 +1,123 @@
+// window.addEventListener("load", () => {
+// Possible improvements:
+// - Change timeline and volume slider into input sliders, reskinned
+// - Change into Vue or React component
+// - Be able to grab a custom title instead of "Music Song"
+// - Hover over sliders to see preview of timestamp/volume change
+import { selectSong } from "./songlist.js";
+const audioPlayer = document.querySelector(".audio-player");
+const songTitle = document.querySelector(".name");
+
+let queue;
+let index;
+let audio = new Audio();
+
+export function isPlaying(id) {
+  return queue && index < queue.length && queue[index].id == id;
+}
+
+audio.addEventListener(
+  "loadeddata",
+  () => {
+    audioPlayer.querySelector(".time .length").textContent = getTimeCodeFromNum(
+    audio.duration);
+  
+    audio.volume = .75;
+  },
+  false);
+
+export function queueList(q, offset) {
+  queue = q;
+  index = offset - 1;
+  playNext();
+}
+
+function playNext() {
+    if (index < queue.length) {
+      songTitle.textContent = queue[index].title;
+      audio.src=queue[index].songUrl;
+      selectSong(queue[index].id);
+      if (audio.paused) {
+        playBtn.click();
+      }
+    }
+}
+
+
+//click on timeline to skip around
+const timeline = audioPlayer.querySelector(".timeline");
+timeline.addEventListener("click", e => {
+  const timelineWidth = window.getComputedStyle(timeline).width;
+  const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+  audio.currentTime = timeToSeek;
+}, false);
+
+//click volume slider to change volume
+const volumeSlider = audioPlayer.querySelector(".controls .volume-slider");
+volumeSlider.addEventListener('click', e => {
+  const sliderWidth = window.getComputedStyle(volumeSlider).width;
+  const newVolume = e.offsetX / parseInt(sliderWidth);
+  audio.volume = newVolume;
+  audioPlayer.querySelector(".controls .volume-percentage").style.width = newVolume * 100 + '%';
+}, false);
+
+//check audio percentage and update time accordingly
+setInterval(() => {
+  if (audio) {
+    const progressBar = audioPlayer.querySelector(".progress");
+    progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
+    audioPlayer.querySelector(".time .currentTime").textContent = getTimeCodeFromNum(
+    audio.currentTime);
+    if (progressBar.style.width == '100%') {
+      index++;
+      playNext();
+    }
+  }
+
+}, 500);
+
+//toggle between playing and pausing on button click
+const playBtn = audioPlayer.querySelector(".controls .toggle-play");
+playBtn.addEventListener(
+"click",
+() => {
+  if (audio.paused) {
+    playBtn.classList.remove("play");
+    playBtn.classList.add("pause");
+    audio.play();
+  } else {
+    playBtn.classList.remove("pause");
+    playBtn.classList.add("play");
+    audio.pause();
+  }
+},
+false);
+
+
+
+audioPlayer.querySelector(".volume-button").addEventListener("click", () => {
+  const volumeEl = audioPlayer.querySelector(".volume-container .volume");
+  audio.muted = !audio.muted;
+  if (audio.muted) {
+    volumeEl.classList.remove("icono-volumeMedium");
+    volumeEl.classList.add("icono-volumeMute");
+  } else {
+    volumeEl.classList.add("icono-volumeMedium");
+    volumeEl.classList.remove("icono-volumeMute");
+  }
+});
+
+//turn 128 seconds into 2:08
+function getTimeCodeFromNum(num) {
+  let seconds = parseInt(num);
+  let minutes = parseInt(seconds / 60);
+  seconds -= minutes * 60;
+  const hours = parseInt(minutes / 60);
+  minutes -= hours * 60;
+
+  if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+  return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+  seconds % 60).
+  padStart(2, 0)}`;
+}
+// })
