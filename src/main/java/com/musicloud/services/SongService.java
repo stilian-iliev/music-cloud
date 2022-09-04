f4d89a03dd9c38b725ca3e6ec8c2d082f88ded9a
stilian-iliev <spr1nkler@protonmail.com>
2022-09-04 19:20:21 +0300

    test

--- src/main/java/com/musicloud/services/SongService.java
+++ src/main/java/com/musicloud/services/SongService.java
@@ -82,6 +82,7 @@ public class SongService {
     }
 
     public List<SongDto> getAllSongs(String query) {
+        //test
         if (query == null) return getAllSongs();
         return songRepository.findAllByMatching(query).stream().map(SongDto::new).collect(Collectors.toList());
     }
